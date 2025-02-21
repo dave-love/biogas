1f15e2aab6de37b8080524638df09f65d9c97597
Dave Love <68555808+dave-love@users.noreply.github.com>
2025-02-21 11:28:22 -0500

    Update figure5.R

--- figure5.R
+++ figure5.R
@@ -3,15 +3,21 @@
 #Data from Table 5-2:  Emissions from Agriculture (kt) on pg3 of the document
 #https://www.epa.gov/system/files/documents/2024-04/us-ghg-inventory-2024-chapter-5-agriculture.pdf
 
-df <- read.csv("https://raw.githubusercontent.com/dave-love/biogas/main/epa_2022_ag_emissions.csv") %>% 
-  mutate(Gas = fct_relevel(Gas,"N2O", "CO2", "CH4"),
-         Source = fct_reorder(Source, -Amount))
 
+library(ggplot2)
+library(dplyr)
+library(forcats)
 
-p <-  ggplot(df, aes(x=Source, y= Amount, fill=Gas)) +
-  geom_bar(stat="identity") +
-  scale_fill_brewer(palette="PuRd")+
-  labs(x="Source",y="Emissions (MMT CO2 eq)")+
+# Load data
+df <- read.csv("https://raw.githubusercontent.com/dave-love/biogas/main/epa_2022_ag_emissions.csv") %>%
+  mutate(Gas = factor(Gas, levels = c("N2O", "CO2", "CH4")),  # Ensure factor order
+         Source = fct_reorder(factor(Source), -Amount))       # Order by emissions
+
+# Create plot
+p <- ggplot(df, aes(x = Source, y = Amount, fill = Gas)) +
+  geom_bar(stat = "identity") +
+  scale_fill_brewer(palette = "PuRd") +
+  labs(x = "Source", y = "Emissions (MMT CO2 eq)", fill = "Gas") +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         legend.position="right",
@@ -20,4 +26,5 @@ p <-  ggplot(df, aes(x=Source, y= Amount, fill=Gas)) +
         strip.background =element_blank(),
         axis.text.x = element_text(angle = 45, hjust=1))
 
+# Print plot
 print(p)
